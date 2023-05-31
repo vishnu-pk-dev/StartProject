d94fac497aae7b2ae9b61fe59fcb908f99536952
mac <mac@macs-MacBook-Pro.local>
2023-05-31 19:41:09 +0530

    eod 31-5

--- start-project/src/app/components/Section2.tsx
+++ start-project/src/app/components/Section2.tsx
@@ -3,22 +3,15 @@ import Image from "next/image";
 import { useEffect } from "react";
 import vector16 from "../../../public/Vector-16.svg";
 import SlidingCards from "./SlidingCards";
-export default function Section2() {
-  useEffect(() => {
-    console.log("inside useeffect");
-    window.addEventListener("scroll", scrollFun);
-    return () => {
-      window.removeEventListener("scroll", scrollFun);
-    };
-  }, []);
-  const scrollFun = () => {
-    console.log("scrollinggg");
-    const vector = document.querySelector(".vector162");
-    console.log(vector);
-  };
+export default function Section2({ scale }: { scale: number }) {
   return (
     <section id="section-2">
-      <Image className="vector162" src={vector16} alt="vector16" />
+      <Image
+        className="vector162"
+        src={vector16}
+        alt="vector16"
+        style={{ scale: scale }}
+      />
       <div className="overflow-contain">
         <div className="rewards-container">
           <div className="btn-div">Our goal is simple:</div>
--- start-project/src/app/page.tsx
+++ start-project/src/app/page.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useState } from "react";
 // import Image from 'next/image'
 // import logo from '../../public/START_logo_fullwhite.svg'
 // import logo3d from '../../public/start-3D.svg'
@@ -8,62 +9,31 @@
 // import mouseScroll from '../../public/Mouse-icon.svg'
 import Section1 from "./components/Section1";
 import Section2 from "./components/Section2";
-
+let i = 1;
+let scrllY: any = 0;
 export default function Home() {
+  const [scale, setScale] = useState(1);
+  function myFun() {
+    const element = document.querySelector("#section-2");
+    let rect: any = element?.getBoundingClientRect();
+    if (rect?.y < scrllY && i <= 2) {
+      i += 0.01;
+      setScale(i);
+    } else if (i >= 1) {
+      i -= 0.01;
+      setScale(i);
+    }
+    scrllY = rect?.y;
+  }
   return (
     <div
       className="home"
       onScroll={() => {
-        console.log("scrollinggg");
+        myFun();
       }}
     >
       <Section1 />
-      <Section2 />
+      <Section2 scale={scale} />
     </div>
-    //  <section id='section-1' className='header-section'>
-    //   <div className='bg-elements'>
-    //     <h1 className='bg-elements-text'>START</h1>
-    //     <div className='bg-elements-grad'></div>
-    //     </div>
-    //   <div className='container'>
-    //     <nav>
-    //       <div><Image className='logo' src={logo} alt="logo" /></div>
-    //       <div className='nav-links'>
-    //         <a href="">Rewards</a>
-    //         <a href="">Contact us</a>
-    //         <a href="">FAQs</a>
-    //       </div>
-    //       <button>
-    //         Sign in
-    //       </button>
-    //     </nav>
-    //     <div className='main'>
-    //     <div className='main-content'>
-    //       <h3 className='q-heading'>Buying a property?</h3>
-    //       <p className='q-answer'>The only thing left to sweeten the deal is our</p>
-    //       <h2 className='year-end'>Year End</h2>
-    //       <Image className='logo-3d' src={logo3d} alt="logo3D" />
-    //       <div className='flex-container'>
-    //         <div className='h-line'></div>
-    //         <p className='offer-text'>Festive offers are back</p>
-    //       </div>
-    //       <p className='promo-text'>Enjoy special promos and download cashback vouchers that will help you EARN more savings from participating projects and household brands.</p>
-    //         <button>
-    //         <Image className='button-ico' src={arrowRuond} alt="arrowRound"/>
-    //         <p>Explore rewards</p>
-    //         </button>
-    //     </div>
-    //     <div className='main-img-right'>
-    //     <Image className='img-right' src={women1} alt="women-1"/>
-    //     </div>
-    //     </div>
-    //     <div className='mouse-scroll'>
-    //       <p className='mouse-scroll-text'>Scroll down</p>
-    //       <div className='mouse-scroll-icon'>
-    //         <Image src={mouseScroll} alt="mouse-scroll"/>
-    //       </div>
-    //     </div>
-    //   </div>
-    //  </section>
   );
 }
